f37a0263ef3941cf04f8e3ab3c241eb74e4094b2
diyor0413 <diyorkhol9@gmail.com>
2023-10-14 18:05:19 +0500

    changes

--- scrapingfile/euro_exchange_rate.py
+++ scrapingfile/euro_exchange_rate.py
@@ -1,4 +1,4 @@
-#Scraping currency exchange rate from Bank.uz
+# Scraping currency exchange rate from Bank.uz
 import json
 from datetime import datetime
 import time
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import aiohttp
 import asyncio
 from fake_useragent import UserAgent
+
 
 async def get_currency_exchange():
     ua = UserAgent()
@@ -17,39 +18,37 @@ async def get_currency_exchange():
         response = await session.get(url="https://bank.uz/currency", headers=headers)
         soup = BeautifulSoup(await response.text(), 'lxml')
 
-
         currency_rate_content = soup.find_all("div", id="best_EUR")
         currency_dict = {}
         for currency_r in currency_rate_content:
             currency_name = 'EUR'
-            currency_purchase = [item.text.strip() for item in currency_r.find(class_='bc-inner-block-left').find_all(class_='bc-inner-block-left-texts')]
-            currency_sale = [item1.text.strip() for item1 in currency_r.find(class_='bc-inner-blocks-right').find_all(class_='bc-inner-block-left-texts')]
+            currency_purchase = [item.text.strip() for item in currency_r.find(class_='bc-inner-block-left').find_all(
+                class_='bc-inner-block-left-texts')]
+            currency_sale = [item1.text.strip() for item1 in currency_r.find(class_='bc-inner-blocks-right').find_all(
+                class_='bc-inner-block-left-texts')]
             currency_url = 'https://bank.uz/currency'
 
-
-
-
-            currency_dict={
+            currency_dict = {
                 'currency_name': currency_name,
                 'currency_purchase': currency_purchase,
                 'currency_sale': currency_sale,
                 'currency_url': currency_url
             }
-        #currency_purchase = soup.find(class_='bc-inner-blocks-right').find_all(class_='bc-inner-block-left-texts')
+        # currency_purchase = soup.find(class_='bc-inner-blocks-right').find_all(class_='bc-inner-block-left-texts')
 
-        #print(currency_purchase.text.strip())
-        #for i in currency_purchase:
+        # print(currency_purchase.text.strip())
+        # for i in currency_purchase:
         #    print(i.text.strip())
-    #print(currency_rate_content)
+    # print(currency_rate_content)
     with open('scrapingfile/euro_exchange_rate.json', 'w', encoding='utf-8') as file:
         json.dump(currency_dict, file, indent=4, ensure_ascii=False)
-
 
 
 async def main():
     await get_currency_exchange()
 
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 loop.run_until_complete(get_currency_exchange())
